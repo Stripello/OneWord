ff2b1b74b6f0b9b0feba291b0585972ce1775614
Владлен Шапарев <shve@outlook.com>
2022-04-22 18:58:27 +0700

    Add project files.

---
+++ Number.cs
@@ -0,0 +1,48 @@
+﻿namespace OneWord
+{
+    internal class Number
+    {
+        internal static int AskOne()
+        {
+            do
+            {
+                Console.WriteLine("Please, enter the number.");
+                var userAnswer = Console.ReadLine();
+                if (uint.TryParse(userAnswer, out uint result) && result > 1)
+                {
+                    return (int)result;
+                }
+                Console.WriteLine("Can't parse entered string to integer number greater than 1");
+            }
+            while (true);
+        }
+
+        internal static (int first, int second) AskPair()
+        {
+            uint first;
+            do
+            {
+                Console.WriteLine("Please, enter first number.");
+                var userAnswer = Console.ReadLine();
+                if (uint.TryParse(userAnswer, out first))
+                {
+                    break;
+                }
+                Console.WriteLine("Can't parse entered string to unsighn integer value.");
+            }
+            while (true);
+
+            do
+            {
+                Console.WriteLine("Please, enter second number.");
+                var userAnswer = Console.ReadLine();
+                if (uint.TryParse(userAnswer, out uint second))
+                {
+                    return ((int)first, (int)second);
+                }
+                Console.WriteLine("Can't parse entered string to unsighn integer value.");
+            }
+            while (true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using OneWord;
+
+Console.WriteLine("Enter word.");
+var word = Console.ReadLine();
+Console.WriteLine(word.Reverse().ToArray());
+
+var pair = Number.AskPair();
+var X = pair.first;
+var Y = pair.second;
+
+var length = word.Length;
+Console.WriteLine("Counting of symbols in word begins from 0.");
+if (length-1 < X)
+{
+    Console.WriteLine($"Can't show you symbol number {X} because entered word is too short.");
+}
+else
+{
+    Console.WriteLine($"Char number {X} = {word[X]}");
+}
+
+if (length - 1 >= Y)
+{
+    var isEqual = word[X].Equals(word[Y]);
+    if (isEqual)
+    {
+        Console.WriteLine($"Symbols {X} and {Y} is eqal.");
+    }
+    else
+    {
+        Console.WriteLine($"Symbols {X} and {Y} are not euqal.");
+    }
+    Console.WriteLine($"Concatination of this two symbols = {word[X]}{word[Y]}.");
+}
+else
+{
+    Console.WriteLine($"Can't definite equity of symbol {X} and {Y} because entered word is too short.");
+}
+
+X = Number.AskOne();
+var i = 0;
+var numbers = new List<int>();
+do
+{
+    var temp =(int) Math.Pow(X, i);
+    if (temp <= length - 1)
+    {
+        numbers.Add(temp);
+        i++;
+    }
+    else
+    {
+        break;
+    }
+}
+while (true);
+
